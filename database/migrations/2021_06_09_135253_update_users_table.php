7ca74cb2a17694c114ab40fd56153e16741171cb
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-06-09 16:28:34 +0200

    Inserimento migration tabella users e login, mancante specializzazione

---
+++ database/migrations/2021_06_09_135253_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('lastname', 25)->after('firstname');
+            $table->string('address', 75)->after('lastname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('lastname');
+            $table->dropColumn('address');
+        });
+    }
+}
