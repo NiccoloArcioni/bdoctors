98977d6799117de069c713809c95cb6ce3b86802
Tsvetan <trendjazzer@gmail.com>
2021-06-11 12:39:28 +0200

    login logout routes on nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,30 +4,33 @@
           <div class="d-flex align-items-center">
             <i class="icofont-mail"></i> <a href="mailto:support@bdoctors.com">Serve aiuto? Clicca qui per contattare il nostro team di supporto</a> 
           </div>
-          {{-- <div class="d-flex align-items-center">
-            <i class="icofont-phone"></i> Call us now +1 5589 55488 55
-          </div> --}}
         </div>
     </div>
   <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
-
       <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <h1 class="logo mr-auto"><a href="index.html">Medicio</a></h1> -->
-
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="index.html">Home</a></li>
           <li><a href="#about">Su di noi</a></li>
           <li><a href="#services">Servizi</a></li>
+          @if(Auth::user() == NULL)
+          <li> <a class="login-btn" href="{{route('login')}}"><i class="icofont-ui-user"></i> Login</a></li>
+          @else
+          <li> <a class="login-btn" href="{{ url('/home') }}"><i class="icofont-ui-user"></i> profilo</a></li>
+          @endif
         </ul>
       </nav>
-      <!-- .nav-menu -->
-      {{-- <a href="#appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">Make an</span> Appointment</a> --}}
-      <a href="#appointment" class="login-btn scrollto"><i class="icofont-ui-user"></i> Login</a>
-      <a href="#appointment" class="register-btn scrollto">Sei un dottore? Iscriviti</a>
-
+      @if(Auth::user() == NULL)
+      <a href="{{route('register')}}" class="register-btn scrollto">Sei un dottore? Iscriviti</a>
+      @else
+      <a href="{{ route('logout') }}" class="register-btn scrollto"
+      onclick="event.preventDefault();
+      document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+      @endif
+      <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+        @csrf
+      </form>
     </div>
   </header><!-- End Header -->
