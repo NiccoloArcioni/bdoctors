9e3fad41ee8cabd5170497c058ed3ca40ea48ea4
Laura <laura.dionisi91@gmail.com>
2021-06-17 10:12:13 +0200

    Card search doctors

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,13 +1,65 @@
-@extends('layouts.app')
-
 @section('content')
-    <h2>specialization</h2>
-    @dump($specialization)
-    <h2>doctors</h2>
-    @dump($doctors)
+    <h2>Tutti gli specialisti</h2>
+    {{-- @dump($doctors)
     @foreach ($doctors as $doctor)
       <h2>{{ $doctor->firstname }}</h2>
       @dump($doctor)
       @dump($doctor->specializations)
+    @endforeach --}}
+    @foreach ($doctors as $doctor)
+    <div class="page-content page-container" id="page-content">
+      <div class="padding">
+          <div class="row container d-flex">
+              <div class="col-xl-12 col-md-12">
+                  <div class="card user-card-full">
+                      <div class="row m-l-0 m-r-0">
+                          <div class="col-sm-4 bg-c-lite-green user-profile">
+                              <div class="card-block text-center text-white">
+                                  <div class="m-b-25"> <img src="{{asset('storage/' . $doctor->photo)}}" class="img-radius" alt="User-Profile-Image"> </div>
+                                  <h6 class="f-w-600">{{ $doctor->firstname }}{{ $doctor->lastname }}</h6>
+                                  @foreach ($doctor->specializations as $specialization)
+                                    <p>{{ $specialization->specialization }}</p> <i class=" mdi mdi-square-edit-outline feather icon-edit m-t-10 f-16"></i>
+                                  @endforeach
+                                  
+                              </div>
+                          </div>
+                          <div class="col-sm-8">
+                              <div class="card-block">
+                                  <h6 class="m-b-20 p-b-5 b-b-default f-w-600">Information</h6>
+                                  <div class="row">
+                                      <div class="col-sm-6">
+                                          <p class="m-b-10 f-w-600">Email</p>
+                                          <h6 class="text-muted f-w-400">{{ $doctor->email }}</h6>
+                                      </div>
+                                      <div class="col-sm-6">
+                                          <p class="m-b-10 f-w-600">Indirizzo</p>
+                                          <h6 class="text-muted f-w-400">{{ $doctor->address }}</h6>
+                                      </div>
+                                  </div>
+                                  <h6 class="m-b-20 m-t-40 p-b-5 b-b-default f-w-600">Projects</h6>
+                                  <div class="row">
+                                      <div class="col-sm-6">
+                                          <p class="m-b-10 f-w-600">City</p>
+                                          <h6 class="text-muted f-w-400">{{ $doctor->city }}</h6>
+                                      </div>
+                                      <div class="col-sm-6">
+                                          <p class="m-b-10 f-w-600">Prestazioni</p>
+                                          <h6 class="text-muted f-w-400">{{ $doctor->performance }}</h6>
+                                      </div>
+                                  </div>
+                                  <ul class="social-link list-unstyled m-t-40 m-b-10">
+                                      <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="facebook" data-abc="true"><i class="mdi mdi-facebook feather icon-facebook facebook" aria-hidden="true"></i></a></li>
+                                      <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="twitter" data-abc="true"><i class="mdi mdi-twitter feather icon-twitter twitter" aria-hidden="true"></i></a></li>
+                                      <li><a href="#!" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="instagram" data-abc="true"><i class="mdi mdi-instagram feather icon-instagram instagram" aria-hidden="true"></i></a></li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
     @endforeach
+    
 @endsection
