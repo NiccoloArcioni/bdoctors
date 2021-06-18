17dede695b9e40f00982611b575cd0009abcc315
GFesta <giuseppefesta89@gmail.com>
2021-06-18 16:27:24 +0200

    correzione pagina responsive

--- resources/views/doctor_detail.blade.php
+++ resources/views/doctor_detail.blade.php
@@ -2,40 +2,46 @@
 
 
 @section('content')
-    <main>
+    <div id="bg">
         <div class="container-single">
-            <div class="col-lg-8">
-                <div class="header-doc">
-                    <div class="foto">
-                        <img src=" {{asset('storage/' . $doctor->photo)}}" alt="">
+            <div class="row">
+                <div class="col-lg-8 col-sm-12">
+                    <div class="header-doc">
+                        <div class="foto">
+                            <img src=" {{asset('storage/' . $doctor->photo)}}" alt="">
+                        </div>
+                        <div class="col-doc">
+                            <h1>{{$doctor->firstname}} {{$doctor->lastname}}</h1>
+                            
+                            @foreach ($doctor->specializations as $specialization)
+                                <h2> {{$specialization->specialization}} </h2>
+                            @endforeach
+                        </div>
                     </div>
-                    <div class="col-doc">
-                        <h1>{{$doctor->firstname}} {{$doctor->lastname}}</h1>
-                        
-                        @foreach ($doctor->specializations as $specialization)
-                            <h2> {{$specialization->specialization}} </h2>
-                        @endforeach
-                    </div>
-                </div>
 
-                <div class="cv">
-                    {{-- provvisorio --}}
-                    <h3>Esperienza</h3>
-                    <p> {{$doctor->cv}} </p>
-                    <a class="btn btn-info" href="#disponibilita">Contatta il medico {{$doctor->lastname}} </a>
+                    <div class="cv">
+                        {{-- provvisorio --}}
+                        <h3>Esperienza</h3>
+                        <p> {{$doctor->cv}} </p>
+                        <a class="btn btn-info" href="#disponibilita">Contatta il medico {{$doctor->lastname}} </a>
+                    </div>
                 </div>
-            </div>
 
                 {{-- INFO --}}
-                <div style="align-self: center" class="col-lg-4">
+                <div style="align-self: center" class="col-lg-4 col-sm-12">
                     <div class="info">
                         <h2>Informazioni generali</h2>
                         <p> Telefono: {{$doctor->telephone}}</p>
                         <p> Indirizzo: {{$doctor->address}} - {{$doctor->city}}</p>
+                    
+                        {{-- @foreach ($profile->performance as $item)
+                            <p>Prestazione:{{$item->performance}}</p>
+                        @endforeach --}}
+
         
         
-                    {{-- RECENSIONI  --}}
-                    <h2>Recensioni</h2>
+                        {{-- RECENSIONI  --}}
+                        <h2>Recensioni</h2>
                         <p>Bravo il dottore</p>
                     {{-- @foreach ($rev as $revs)
                         @for ($i = 0; $i < $revs->vote_user; $i++)
@@ -78,6 +84,7 @@
                             </div>
                             <button type="submit" class="btn btn-info">Invia</button>
                             </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -120,5 +127,5 @@
                 <img src=" {{ asset('images/doc-message.png') }} " alt="">
             </div>
         </div>
-    </main>           
+    </div>           
 @endsection
