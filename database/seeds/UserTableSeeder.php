ca5a2e18f476f1e98d6eaa190f21193b73ad485b
Niccolò Arcioni <niccoloarcioni@gmail.com>
2021-06-22 18:48:07 +0200

    seed performance

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -16,7 +16,12 @@ class UserTableSeeder extends Seeder
         /* $doctors = config('doctor'); */
         /* se non funziona config usare include */
         $data = include('config\doctor.php');
-    
+        $performances = [
+            'In studio',
+            'Da remoto',
+            'Sia in studio che da remoto'
+        ];
+
         for ($i = 0; $i < 390; $i++) {
             $new_doctor = new User();
             $new_doctor->firstname = $data['names'][rand(0, 19)];
@@ -27,6 +32,10 @@ class UserTableSeeder extends Seeder
             $new_doctor->email = $new_doctor->firstname . $new_doctor->lastname . rand(1, 20000) . '@gmail.com';
             $new_doctor->password = Hash::make('mario1234');
             $new_doctor->cv = $data['cv'][rand(0, 19)];
+            $rnd_telephone = strval(rand(1111111111, intval(9999999999)));
+            $new_doctor->performance = $performances[rand(0, 2)];
+            $new_doctor->telephone = $rnd_telephone;
+
             $new_doctor->save();
         }
     }
